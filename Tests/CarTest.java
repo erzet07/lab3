d45652540fbabd883b7222ba3ada779bba835ea9
Erik Zetterlund <7anerik@gmail.com>
2023-11-17 12:03:32 +0100

    final version v2

--- Tests/CarTest.java
+++ Tests/CarTest.java
@@ -191,9 +191,10 @@ class CarTest {
 
     @Test
     void testchangeflakVinkel() {
-        scania.changeflakVinkel(50);
+        scania.changeFlak(50);
         assertEquals(50, scania.getflakVinkel());
-        scania.changeflakVinkel(70);
+
+        scania.changeFlak(70);
         assertEquals(70, scania.getflakVinkel());
     }
 
@@ -219,5 +220,6 @@ class CarTest {
     void testGetMaxAntalBilar() {
         int expectedMaxAntalBilar = 4;
         assertEquals(expectedMaxAntalBilar, saabVerkstad.getMaxAntalBilar());
+
     }
 }
